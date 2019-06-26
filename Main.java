b540c935f8e0064dd891064981f128a538b723bf
travelbetagabe247 <51954175+travelbetagabe247@users.noreply.github.com>
2019-06-26 13:12:00 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+package com.travelbetaprogabe;
+
+    public class Main {
+
+        public static void main(String[] args) {
+
+            char copyrightSymbol = '\u00A9';
+            System.out.println("Copyright Symbol = " + copyrightSymbol);
+
+            char registeredSymbol = '\u00AE';
+            System.out.println("Registered Symol = " + registeredSymbol);
+
+            boolean myBoolean = false;
+            boolean isMale = true;
+
+    }
+}
